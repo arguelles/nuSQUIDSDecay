03f341d641678da7730b6f92e843b27d80db73da
Carlos Arguelles <caad@mit.edu>
2016-12-23 13:49:24 -0500

    added license

--- inc/nusquids_decay.h
+++ inc/nusquids_decay.h
@@ -1,5 +1,35 @@
-#ifndef nusquidslv_H
-#define nusquidslv_H
+#ifndef nusquidslv_H #define nusquidslv_H
+
+/*
+Copyright © 2016 Alexander Moss, Marjon Moulai, Carlos Arguelles, and Janet
+Conrad.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+For more information please email:
+
+Alexander Moss (zander@mit.edu)
+Marjon Moulai (marjon@mit.edu)
+Carlos Arguelles (caad@mit.edu)
+Janet Conrad (conrad@mit.edu)
+
+*/
 
 #include <vector>
 #include <iostream>
