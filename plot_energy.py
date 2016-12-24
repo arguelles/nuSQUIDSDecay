f7baad0131a5ca404fa4ff73d521344838104cbd
Zander Moss <zander@mit.edu>
2016-12-24 18:16:58 -0500

    Added utilities for plotting oscillograms. Preparing to check nusquids against nusheep both with and without regeneration.

---
+++ plot_energy.py
@@ -0,0 +1,180 @@
+#! /usr/bin/python
+import matplotlib as mpl
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+from pylab import rcParams
+rcParams['figure.figsize'] = 10, 8
+plt.rc('font',family='Times New Roman')
+plt.rc('font', size=16)
+
+
+#regen_data=np.load("output/regen_orderflip.npz")
+regen_data=np.load("output/test_fresh.npz")
+noregen_data=np.load("output/dcy14_noregen_softail.npz")
+
+regen_energy=regen_data['e']
+regen_flux=regen_data['flux']
+noregen_energy=noregen_data['e']
+noregen_flux=noregen_data['flux']
+
+pi=3.141592
+#-----------------------------------------------------------------#
+#Plotting data
+
+fig,ax = plt.subplots()
+ax.plot(regen_energy,regen_flux[1,0,:],color="red",marker="o",lw=2,ls='-',label="Electron")
+ax.plot(regen_energy,regen_flux[1,1,:],color="green",marker="o",lw=2,ls='-',label="Muon")
+ax.plot(regen_energy,regen_flux[1,2,:],color="blue",marker="o",lw=2,ls='-',label="Tau")
+ax.plot(regen_energy,regen_flux[1,3,:],color="purple",marker="o",lw=2,ls='-',label="Sterile")
+#ax.plot(noregen_energy,noregen_flux[1,2,:],color="blue",lw=2,marker="o",ls='-',label="Without Regeneration")
+plt.xscale('log')
+#ax.set_ylim([0,1e-10])
+#ax.set_xlim([regen_energy[0],1.2e2])
+
+ax.set_xlabel("Neutrino Energy [GeV]")
+ax.set_ylabel(r'$\frac{d\Phi}{dE}$')
+ax.legend()
+
+"""
+axes=[]
+
+xax_vector=valvec
+
+axes.append(plt.subplot2grid((4,4),(1,0),rowspan=3,colspan=4))
+if (numcomp==False):
+	axes[0].plot(xax_vector,pert_vec0,color="red",label="Standard Vacuum Solution",lw=2,ls='-')
+#	axes[0].plot(xax_vector,pert_vec1,color="green",label="",lw=2,ls='--')
+#	axes[0].plot(xax_vector,pert_vec2,color="green",label="",lw=2,ls='-.')
+	axes[0].plot(xax_vector,O1_vec,color="green",label="Perturbation O1",lw=2,ls='-')
+	axes[0].plot(xax_vector,O2_vec,color="blue",label="Perturbation O2",lw=2,ls='-')
+#	axes[0].plot(xax_vector,diag_vec,color="black",label="",lw=0,ls='--')
+#	axes[0].plot(xax_vector,diag_vec,color="black",label="",lw=0,ls='-.')
+axes[0].plot(xax_vector,num_vec,color="black",label="Numerical Solution",lw=2,ls='-')
+
+#if (numcomp==True):
+#axes[0].plot(xax_vector,diag_vec,color="orange",label="Diagonal Solution",lw=2,ls='--')
+
+axes[0].set_xlim([xax_vector[0],xax_vector[-1]])
+axes[0].set_ylim([0.45,1.1])
+axes[0].set_xlabel("Neutrino Energy [TeV]",labelpad=0)
+axes[0].set_ylabel("Muon Neutrino Survival Fraction")
+axes[0].yaxis.set_major_formatter(mpl.ticker.ScalarFormatter(useMathText=True, useOffset=False))
+
+axes.append(plt.subplot2grid((4,4),(0,0),colspan=4,sharex=axes[0]))
+
+if (numcomp==False):
+	axes[1].plot(xax_vector,np.divide(np.subtract(pert_vec0,num_vec),num_vec),color="red",lw=2,ls='-')
+#	axes[1].plot(xax_vector,np.divide(np.subtract(pert_vec1,num_vec),num_vec),color="green",lw=2,ls='--')
+#	axes[1].plot(xax_vector,np.divide(np.subtract(pert_vec2,num_vec),num_vec),color="green",lw=2,ls='-.')
+	axes[1].plot(xax_vector,np.divide(np.subtract(O1_vec,num_vec),num_vec),color="green",lw=2,ls='-')
+#axes[1].plot(xax_vector,np.divide(np.subtract(diag_vec,num_vec),num_vec),color="pink",lw=2,ls='-')
+	axes[1].plot(xax_vector,np.divide(np.subtract(O2_vec,num_vec),num_vec),color="blue",lw=2,ls='-')
+axes[1].plot(xax_vector,np.divide(np.subtract(num_vec,num_vec),num_vec),color="black",lw=2,ls='-')
+
+#if (numcomp==True):
+#axes[1].plot(xax_vector,np.divide(np.subtract(num_vec,num_vec),num_vec),color="orange",lw=2,ls='--')
+
+#axes[1].set_yscale('log')
+axes[1].set_xlim([xax_vector[0],xax_vector[-1]])
+axes[1].set_ylabel("Relative Residual")
+axes[1].set_ylim([-0.2,0.2])
+axes[1].yaxis.set_major_formatter(mpl.ticker.ScalarFormatter(useMathText=True, useOffset=False))
+plt.setp(axes[1].get_xticklabels(), visible=False)
+
+for ax in axes:
+	ax.set_xscale('log')
+
+
+
+legends=[]
+frames=[]
+
+single_axis=[]
+single_axis.append(axes[0])
+
+for ax in enumerate(single_axis):
+
+	legends.append(ax[1].legend(loc='lower right', shadow=False,fancybox=True))
+
+	# The frame is matplotlib.patches.Rectangle instance surrounding the legend.
+	frames.append(legends[ax[0]].get_frame())
+	frames[ax[0]].set_facecolor('1.0')
+
+	# Set the fontsize
+	for label in legends[ax[0]].get_texts():
+	    label.set_fontsize(16)
+	
+	for label in legends[ax[0]].get_lines():
+	    label.set_linewidth(2.0)  # the legend line width
+
+for ax in axes:
+	ax.tick_params(axis='x', which='major', width=1,length=5)
+	ax.tick_params(axis='x', which='minor', width=1,length=3)
+	ax.tick_params(axis='y', which='major', width=1,length=5)
+	ax.tick_params(axis='y', which='minor', width=1,length=3)
+
+
+# Get the Bbox
+plt.draw()
+bb = legends[0].legendPatch.get_bbox().inverse_transformed(axes[0].transAxes)
+
+# these are matplotlib.patch.Patch properties
+props = dict(boxstyle='round', facecolor='cyan', alpha=0.2)
+
+
+def extract_man_exp(val):
+	#Extract mantissae and exponents for pretty printing!
+	mystr="%.2e"%(val)
+	mantissa=[]
+	exponent=[]
+	print mystr
+	hit_e=False
+	for c in mystr:
+		if (c=='e'):
+			hit_e=True
+		else:
+			if (hit_e==False):
+				mantissa.append(c)
+			else:
+				exponent.append(c)
+	man_str=''.join(mantissa)
+	exp_str=''.join(exponent)
+	
+	ret=[float(man_str),int(exp_str)]
+	return ret
+		
+
+def pretty_exp_note(parstring,unitstring,val,f_force=False,last=False):
+	if (('e' not in '%g'%(val)) or (f_force==True)):
+		ret = parstring+r'$=%.2f$'%(val)+'  ['+unitstring+']'
+	else:
+		splitval=extract_man_exp(val)
+		ret = parstring+r'$=%.2f \times 10^{%d}$'%(splitval[0],splitval[1])+' ['+unitstring+']'
+	if (last==False):
+		ret+='\n'
+	return ret
+
+
+
+# place a text box relative to the legend
+
+textlist=[
+r'$\phi=\pi/4$  [rad]'+'\n', #Special value for phi
+pretty_exp_note(r'$\theta$','rad',theta,f_force=True),
+pretty_exp_note(r'$V$','TeV',potential/param.TeV),
+pretty_exp_note(r'$\lambda_1$','TeV',l1/param.TeV),
+pretty_exp_note(r'$\lambda_2$','TeV',l2/param.TeV,last=True)]
+textstr=''.join(textlist)
+
+
+axes[0].text(0.25, 0.37, textstr, transform=axes[0].transAxes, fontsize=16,verticalalignment='top', bbox=props)
+"""
+
+plt.show()
+
+
+#np.savez("4TeVnodcy_matter"+str(cmdargs[0])+"timeplot"+str(cmdargs[1]),d=distvec,p=pvec)
